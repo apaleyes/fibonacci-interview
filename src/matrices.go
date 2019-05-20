ebe8b4c903410642c78f07f0f0b9ac2d98b5ff34
Andrei Paleyes <apaleyes@users.noreply.github.com>
2019-05-20 17:27:29 +0100

    Implemented matrix exponentiation approach

--- src/matrices.go
+++ src/matrices.go
@@ -1,6 +1,20 @@
+// This approach is based on the realization that the following formula holds
+//
+// [[0, 1], [1, 1]] * [F_n-1, F_n] = [F_n, F_n+1]
+//
+// To take this further, we have
+//
+// [[0, 1], [1, 1]] ^ n = [[F_n-1, F_n], [F_n, F_n+1]]
+//
+// This allows us to use fast exponentiation (see https://en.wikipedia.org/wiki/Exponentiation_by_squaring#Basic_method)
+// Essentially problem comes down to quickly bringing a certain matrix to the given power
+
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
 
 func multiply_matrices(m1 [2][2]int, m2 [2][2]int) [2][2]int {
     result := [2][2]int{{0, 0}, {0, 0}}
@@ -12,13 +26,29 @@ func multiply_matrices(m1 [2][2]int, m2 [2][2]int) [2][2]int {
     return result
 }
 
-func fib_via_matrix_multiplication(n int) {
+func fast_power(m [2][2]int, n int) [2][2]int {
+    // start with identity matrix
+    result := [2][2]int{{1, 0}, {0, 1}}
+    
+    bits := strconv.FormatInt(int64(n), 2)
+    
+    for _, bit := range bits {
+        result = multiply_matrices(result, result)
+        if (bit == '1') {
+            result = multiply_matrices(result, m)
+        }
+    }
+    
+    return result
+}
 
+func fib_via_matrix_multiplication(n int) int {
+    start := [2][2]int{{0, 1}, {1, 1}}
+    end := fast_power(start, n)
+    return end[1][0]
 }
 
 func main() {
-    m1 := [2][2]int{{0,1},{1,1}}
-    
-    fmt.Println(multiply_matrices(m1, m1))
+    x := fib_via_matrix_multiplication(8)
+    fmt.Printf("This should be 21: %d", x)
 }
-
